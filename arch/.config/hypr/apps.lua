5d36da0587a1718e35f7d07374aa902dd2aaadb3
Horacio <rosendeh@gmail.com>
2026-09-17 14:42:39 -0300

    fix(hypr): center IBM i Access windows in apps.lua

--- arch/.config/hypr/apps.lua
+++ arch/.config/hypr/apps.lua
@@ -4,3 +4,7 @@
 -- even with 14pt fonts, while fitting comfortably on 1.5x scaled laptop screens.
 
 o.window({ tag = "floating-window" }, { size = { 1100, 670 } })
+
+-- IBM i Access Client Solutions (Java Swing / AWT bajo XWayland)
+-- Centrar todas las ventanas de ACS (License Agreement, LaunchPad, Emulador 5250)
+o.window("com-ibm-iaccess-.*", { float = true, center = true })
